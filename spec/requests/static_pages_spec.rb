9f26dc1bfee7a4368a68aa3e0d16e4febdd99ec5
sddtc <changhbaga@gmail.com>
2017-07-10 15:40:28 +0800

    home title test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,8 +15,14 @@ describe 'StaticPages' do
 
   it 'should have the right title' do
     visit '/static_pages/home'
-    page.should have_selector('title',
+    page.should_not have_selector('title',
                               text: "#{base_title} | Home")
+  end
+
+  it "should have the base title" do
+    visit '/static_pages/home'
+    page.should have_selector('title',
+                              :text => "#{base_title}")
   end
 
   describe 'Help page' do
